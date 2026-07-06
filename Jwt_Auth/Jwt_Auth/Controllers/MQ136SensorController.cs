694f4ba99503ae895184f65c86a42bf12c666369
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-06 12:11:09 +0600

    Add Mq136Sensor Controller

--- Jwt_Auth/Jwt_Auth/Controllers/MQ136SensorController.cs
+++ Jwt_Auth/Jwt_Auth/Controllers/MQ136SensorController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jwt_Auth.Models;
+using Jwt_Auth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Jwt_Auth.Controllers
 {
-    public class MQ136SensorController : Controller
-    {
-        public IActionResult Index()
+   
+  
+        [ApiController]
+        [Route("api/[controller]")]
+        [Authorize(Roles = "Admin")]
+        public class MQ136SensorController(IMQ136SensorService service) : ControllerBase
         {
-            return View();
+            [HttpGet]
+            public async Task<IActionResult> GetAll() => Ok(await service.GetAllAsync());
+
+            [HttpGet("{id}")]
+            public async Task<IActionResult> Get(long id)
+            {
+                var sensor = await service.GetByIdAsync(id);
+
+                if (sensor == null)
+                    return NotFound();
+
+                return Ok(sensor);
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Create(MQ136SensorDto dto)
+            {
+                var sensor = await service.AddAsync(dto);
+
+                return Ok(sensor);
+            }
+
+
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> Delete(long id)
+            {
+                if (!await service.DeleteAsync(id))
+                    return NotFound();
+
+                return Ok("Deleted Successfully");
+            }
         }
     }
-}
