2104526d1b1298a34cbc41cc23cb6f513d6cf8aa
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-07 23:19:51 +0600

    Add custom Authentication State Provider

--- Jwt_Auth/Jwt_Auth.Web/Authentication/CustomAuthenticationStateProvider.cs
+++ Jwt_Auth/Jwt_Auth.Web/Authentication/CustomAuthenticationStateProvider.cs
@@ -1,6 +1,59 @@
-﻿namespace Jwt_Auth.Web.Authentication
+﻿using Jwt_Auth.Web.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Jwt_Auth.Web.Authentication
 {
-    public class CustomAuthenticationStateProvider
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
     {
+        private readonly TokenService _tokenService;
+
+        public CustomAuthenticationStateProvider(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _tokenService.GetAccessTokenAsync();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return new AuthenticationState(
+                    new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+
+            var identity = new ClaimsIdentity(jwt.Claims, "jwt");
+
+            var user = new ClaimsPrincipal(identity);
+
+            return new AuthenticationState(user);
+        }
+
+        public async Task NotifyUserAuthentication(string accessToken)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = handler.ReadJwtToken(accessToken);
+
+            var identity = new ClaimsIdentity(jwt.Claims, "jwt");
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(
+                Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void NotifyUserLogout()
+        {
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+            NotifyAuthenticationStateChanged(
+                Task.FromResult(new AuthenticationState(anonymous)));
+        }
     }
 }
