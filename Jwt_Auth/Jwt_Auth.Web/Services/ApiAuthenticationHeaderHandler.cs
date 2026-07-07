ae238f86a79d866b5bf0d80bb673e02f10ea78cb
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-07 23:16:32 +0600

    Add Api Authentication Header Handler

--- Jwt_Auth/Jwt_Auth.Web/Services/ApiAuthenticationHeaderHandler.cs
+++ Jwt_Auth/Jwt_Auth.Web/Services/ApiAuthenticationHeaderHandler.cs
@@ -1,6 +1,25 @@
 ﻿namespace Jwt_Auth.Web.Services
 {
-    public class ApiAuthenticationHeaderHandler
+    public class ApiAuthenticationHeaderHandler : DelegatingHandler
     {
+        private readonly TokenService _tokenService;
+
+        public ApiAuthenticationHeaderHandler(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var token = await _tokenService.GetAccessTokenAsync();
+
+            if(!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
     }
 }
