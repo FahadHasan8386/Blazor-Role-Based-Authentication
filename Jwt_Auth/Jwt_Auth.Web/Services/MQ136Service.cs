bb5575bb334ea806ab8584cdff8f73c1a41bac64
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-08 09:50:45 +0600

    Add MQ136 Sensor Service

--- Jwt_Auth/Jwt_Auth.Web/Services/MQ136Service.cs
+++ Jwt_Auth/Jwt_Auth.Web/Services/MQ136Service.cs
@@ -1,6 +1,36 @@
-﻿namespace Jwt_Auth.Web.Services
+﻿using Jwt_Auth.Shared.Models;
+using System.Net.Http.Json;
+
+namespace Jwt_Auth.Web.Services
 {
     public class MQ136Service
     {
+        private readonly HttpClient _http;
+
+        public MQ136Service(HttpClient http)
+        {
+            _http = http;   
+        }
+
+        public async Task<List<MQ136Service>> GetAllAsync()
+        {
+            var result = await _http.GetFromJsonAsync<List<MQ136Service>>("appi/MQ136Sensor");
+
+            return result ?? new List<MQ136Service>();
+        }
+
+        public async Task<bool> AddAsync(MQ136SensorDto dto)
+        {
+            var response = await _http.PostAsJsonAsync("api/MQ136Sensor", dto);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteAsync(long id)
+        {
+            var response = await _http.DeleteAsync($"api/MQ136Sensor/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
     }
 }
