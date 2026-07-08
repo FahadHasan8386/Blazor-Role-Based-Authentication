8a3588ef2a9feaab68acac7ee172aba8084daa70
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-08 10:08:00 +0600

    add Auth service on web

--- Jwt_Auth/Jwt_Auth.Web/Services/AuthService.cs
+++ Jwt_Auth/Jwt_Auth.Web/Services/AuthService.cs
@@ -1,6 +1,65 @@
-﻿namespace Jwt_Auth.Web.Services
+﻿using Jwt_Auth.Shared.Models;
+using Jwt_Auth.Web.Authentication;
+using System.Net.Http.Json;
+
+namespace Jwt_Auth.Web.Services
 {
     public class AuthService
     {
+        private readonly HttpClient _http;
+        private readonly TokenService _tokenService;
+        private readonly CustomAuthenticationStateProvider _authProvider;
+
+        public AuthService(
+            HttpClient http,
+            TokenService tokenService,
+            CustomAuthenticationStateProvider authProvider)
+        {
+            _http = http;
+            _tokenService = tokenService;
+            _authProvider = authProvider;
+        }
+
+        public async Task<bool> Login(UserDto dto)
+        {
+            var result = await _http.PostAsJsonAsync("api/Auth/login", dto);
+
+            if(!result.IsSuccessStatusCode)
+            {
+                return false;
+            }
+
+            var token = await result.Content.ReadFromJsonAsync<TokenResponseDto>();
+
+            if(token == null)
+            {
+                return false;
+            }
+
+            await _tokenService.SaveTokensAsync(token.AccessToken, token.RefreshToken);
+
+            await _authProvider.NotifyUserAuthentication(token.AccessToken);
+
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            var accessToken = await _tokenService.GetAccessTokenAsync();
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                _http.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer",
+                        accessToken);
+
+                await _http.PostAsync("api/Auth/logout", null);
+            }
+
+            await _tokenService.RemoveTokensAsync();
+
+            _authProvider.NotifyUserLogout();
+        }
     }
 }
